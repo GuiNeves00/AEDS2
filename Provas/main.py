2d098ae0894e270f73ef367211087ca989494b3f
Guilherme Neves <guiinevessilva@gmail.com>
2022-08-24 19:41:23 -0300

    tentando concluir o keysort

--- Provas/main.py
+++ Provas/main.py
@@ -23,18 +23,82 @@ def gera_base_de_dados(nome_arq, obj):
         #for i in range(100):
             #lista.append(obj.gera_funcionario(nome_arq))
     #return lista
+
+def imprime_funcionarios(funcionarios):
+    for i in funcionarios:
+        print(i.cod, "|", i.nome, "|", i.cpf, "|", i.data_nascimento, "|", i.salario)
+
 #------------------------------------------------------------
-lista_func = []
-fm = Func.Funcionario()
 
+aux_dados = Func.Funcionario()
 for i in range(100):
-    lista_func.append(fm.gera_funcionario("data.dat"))
+        aux_dados.gera_funcionario("data.dat")
 
-#lista_func = gera_base_de_dados("data.dat", fm)
+funcionarios = []
+aux_cod = []
+with open("data.dat", "rb+") as arq:
+    for i in arq.readlines():
+        func = Func.Funcionario()
+        i = str(i)
+        aux = i.split("|")
+        func.cod = aux[0][2:]
+        aux_cod.append(aux[0][2:])
+        func.nome = aux[1]
+        func.salario = aux[4]
+        func.cpf = aux[2]
+        func.data_nascimento = aux[3]
+        funcionarios.append(func)
 
-print(lista_func)
-print("\n\n\n")
-lista_func.sort()
-print(sorted(lista_func))
+    codigos=[bin(int(ele)) for ele in aux_cod]
+    codigos=[int(ele,0) for ele in codigos]
+    codigos.sort()
 
-print(fm.busca_codigo(lista_func, str(3)))
+    arq.seek(0)
+    arq.truncate()
+
+    j = 0
+    i = 0
+    while(i < 100):
+        #print("func.cod: ", funcionarios[i].cod, "cod_ord: ", codigos[j])
+        print(i)
+        if int(funcionarios[i].cod, 2) == codigos[j]:
+            print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+            arq.write(str(funcionarios[i].cod).encode())
+            arq.write("|".encode())
+            arq.write(str(funcionarios[i].nome).encode())
+            arq.write("|".encode())
+            arq.write(str(funcionarios[i].cpf).encode())
+            arq.write("|".encode())
+            arq.write(str(funcionarios[i].data_nascimento).encode())
+            arq.write("|".encode())
+            arq.write(str(funcionarios[i].salario).encode())
+            arq.write("|".encode())
+            arq.write("\n".encode())
+            j = j + 1
+            i = j
+        i = i + 1
+
+imprime_funcionarios(funcionarios)
+print("---------------------------------------------------------------------")
+print(codigos)
+
+# print("antes")
+# print(aux_cod)
+# print("depois")
+# codigos.sort()
+# print(codigos)
+
+
+
+# def sortfunc(funcionario):
+#     return funcionario.cod
+
+# sorted(funcionarios, key=sortfunc)
+
+# for i in range(len(funcionarios)):
+#     numero = int(funcionarios[i].cod)
+#     print(f"func: {numero}")
+
+
+#for i in funcionarios:
+#    print(i.cod, "|", i.nome, "|", i.cpf, "|", i.data_nascimento, "|", i.salario)
