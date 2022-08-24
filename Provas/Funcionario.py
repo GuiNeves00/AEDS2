7bc4dbe9158776fc0de2d671c92cc73cceb905e9
Guilherme Neves <guiinevessilva@gmail.com>
2022-08-24 17:16:02 -0300

    v2.0 letra a) e b)

--- Provas/Funcionario.py
+++ Provas/Funcionario.py
@@ -8,6 +8,7 @@
 
 import names
 import random
+import time
 
 class Funcionario:
     def __init__(self, cod = 0, nome = None, cpf = 0, data_nascimento = 0, salario = 0):
@@ -23,47 +24,75 @@ class Funcionario:
     def gera_funcionario(self, nome_arq):
         aux = 0
         arq_funcao = open(nome_arq, "rb+")
-        arq_funcao.seek(0, 2)
+        arq_funcao.seek(0, 2) #!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FIX
+
         #cod
         self.cod = random.choice(self.rnd_array) #escolhe um valor aleatorio do vetor rnd_array
         self.rnd_array.remove(self.cod) #remove este valor do vetor rnd_array para que nao haja codigos repetidos
         #self.cod = str(random.randint(0, 99))  #versao antiga
-        arq_funcao.write(bin(self.cod)[2:].encode()) 
+        arq_funcao.write(bin(self.cod)[2:].encode()) #[2:] para retirar o indicador de binario (0b)
         arq_funcao.write("|".encode())
+
         #nome
         self.nome = (names.get_full_name()) 
         arq_funcao.write(self.nome.encode())
         arq_funcao.write("|".encode())
+
         #cpf
-        self.cpf = (str(random.randint(10000000000, 99999999999)))
-        arq_funcao.write(self.cpf.encode())
+        self.cpf = (random.randint(10000000000, 99999999999))
+        arq_funcao.write(bin(self.cpf)[2:].encode())
         arq_funcao.write("|".encode())
+
         #data_nascimento
-        self.data_nascimento = (str(random.randint(1, 31)) + "/" + str(random.randint(1, 12)) + "/" + str(random.randint(1930, 2022)))
-        arq_funcao.write(self.data_nascimento.encode())
+        dn_dia = random.randint(1, 31)
+        dn_mes = random.randint(1, 12)
+        dn_ano = random.randint(1930, 2022)
+        arq_funcao.write(bin(dn_dia)[2:].encode())
+        arq_funcao.write("/".encode())
+        arq_funcao.write(bin(dn_mes)[2:].encode())
+        arq_funcao.write("/".encode())
+        arq_funcao.write(bin(dn_ano)[2:].encode())
+        arq_funcao.write("/".encode())
         arq_funcao.write("|".encode())
+        self.data_nascimento = str(dn_dia) + "/" + str(dn_mes) + "/" + str(dn_ano)
+
+        #self.data_nascimento = (str(random.randint(1, 31)) + "/" + str(random.randint(1, 12)) + "/" + str(random.randint(1930, 2022)))
+        #arq_funcao.write(self.data_nascimento.encode())
+        #arq_funcao.write("|".encode())
+
         #salario
-        self.salario = (str(random.randint(1200, 100000)))
-        arq_funcao.write(self.salario.encode())
+        self.salario = (random.randint(1200, 100000))
+        arq_funcao.write(bin(self.salario)[2:].encode())
         arq_funcao.write("|".encode())
+
         #fim do registro
-        aux = str(str(self.cod)+"|"+self.nome+"|"+self.cpf+"|"+self.data_nascimento+"|"+self.salario) #str(self.cod) pq sem isso acusou erro
+        aux = str(str(self.cod)+"|"+str(self.nome)+"|"+str(self.cpf)+"|"+str(self.data_nascimento)+"|"+str(self.salario)) #str(self.cod) pq sem isso acusou erro
         arq_funcao.write("\n".encode())
-        arq_funcao.seek(0, 2)
+        arq_funcao.seek(0, 2)#!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FIX
         arq_funcao.close()
         return aux
 
 
     #letra b)
-    def busca_codigo(self, lista, cod):
+    def busca_codigo(self, lista, cod: str):
+        #cod = cod[2:] #retira sufixo indicativo de binario 0b||1b
         comparacoes = 0
+        start_time = time.time() #Inicia o timer para contar o tempo de execucao do algoritmo
+        
         for i in range(100):
+            if "|" in lista[i][1]: #checa se o numero eh de um digito e faz a correcao de busca
+                if cod == lista[i][0]:
+                    print("Dados do funcionario: ", lista[i], "\nTotal de comparacoes: ", comparacoes, "\nTempo gasto: %.10f" % (time.time() - start_time))
+                    return lista[i]
             if cod == lista[i][:2]:
-                print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-                print("Dados do funcionario: ", lista[i], "\nTotal de comparacoes: ", comparacoes)
+                print(lista[i][:2])
+                print("Dados do funcionario: ", lista[i], "\nTotal de comparacoes: ", comparacoes, "\nTempo gasto: %.10f" % (time.time() - start_time))
                 return lista[i]
             else:
                 comparacoes = comparacoes + 1
+            if i == 99:
+                return "Codigo de funcionario invalido ou inexistente!"
+            
 
             #arq_funcao.write(names.get_full_name() + "@") #nome
             #arq_funcao.write(str(random.randint(10000000000, 99999999999))) #cpf
--- Provas/main.py
+++ Provas/main.py
@@ -1,26 +1,40 @@
+from posixpath import split
 import Funcionario as Func
 import names
-
-##arq = open("data.dat", "a+")
-
+import os
+#FUNCOES-----------------------------------------------------
+def gera_base_de_dados(nome_arq, obj):
+    lista = []
+    if os.stat(nome_arq).st_size > 0:
+        print("Base de dados ja foi criada anteriormente!!")
+        opcao = int(input(print("Digite:\n1 = Criar nova base de dados\n2 = Finalizar programa")))
+        if opcao == 1:
+            f = open(nome_arq, "r+")
+            f.seek(0)
+            f.truncate()
+            f.close()
+            
+            for i in range(100):
+                lista.append(obj.gera_funcionario(nome_arq))
+                return lista
+        if opcao == 2:
+            exit()
+    #else:
+        #for i in range(100):
+            #lista.append(obj.gera_funcionario(nome_arq))
+    #return lista
+#------------------------------------------------------------
 lista_func = []
 fm = Func.Funcionario()
 
 for i in range(100):
     lista_func.append(fm.gera_funcionario("data.dat"))
-    ###arq.seek(0, 2)
+
+#lista_func = gera_base_de_dados("data.dat", fm)
 
 print(lista_func)
-print("------------------------------------------------------------")
+print("\n\n\n")
+lista_func.sort()
+print(sorted(lista_func))
 
-print(fm.busca_codigo(lista_func, str(24)))
-
-
-#for i in range(100):
-#    FuncMain[i].gera_funcionario("data.dat")
-
-#FuncMain[0].gera_funcionario("data.dat")
-#FuncMain[1].gera_funcionario("data.dat")
-#FuncMain[2].gera_funcionario("data.dat")
-
-###arq.close()
+print(fm.busca_codigo(lista_func, str(3)))
